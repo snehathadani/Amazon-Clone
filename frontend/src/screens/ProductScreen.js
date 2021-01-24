4fe1b51029c3249d124b45900c3b94496bfb8f99
Sneha Thadani <snehacopper@gmail.com>
2021-01-24 12:21:31 -0800

    added product screen component with props passed from data

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,58 @@
 import React from 'react'
-
-export default function ProductScreen() {
+import Product from '../components/Product';
+import Rating from '../components/Rating';
+import data from '../data';
+export default function ProductScreen(props) {
+    const product = data.products.find(element=> element._id === props.match.params.id) 
+    if(!product) {
+        return <div> Product Not Found</div>;
+    }
     return (
-        <div>
-            ProductScreen
+        <div className ="row">
+            <div className ="col-2" >
+                <img className ="large" src ={product.image} alt ={product.name} />
+            </div>
+            <div className = "col-1" >
+                <ul>
+                    <li>
+                        <h1>{product.name}</h1>
+                    </li>
+                    <li>
+                        <Rating rating = {product.rating} numReviews ={product.numReviews}/>
+                    </li>
+                    <li>Price: ${product.price}</li>
+                    <li><p>Description: {product.description}</p></li>
+                </ul>
+            </div>
+            <div className = "col-1" >
+                <div>
+                    <div className ="card card-body">
+                        <ul>
+                            <li>
+                                <div className = "row" >
+                                    <div>Price</div>
+                                    <div className="price"> ${product.price}</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className = "row" >
+                                    <div>Price</div>
+                                    <div className="price"> {product.countInStock>0? 
+                                    (
+                                    <span className="success">In Stock</span>
+                                    ) : (
+                                        <span className="error"> Unavailable</span>
+                                    )}
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <button className="primary block">Add to Cart</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
