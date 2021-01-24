a90530fd10d4a29e64725a67deaa7c47c7e8d757
Sneha Thadani <snehacopper@gmail.com>
2021-01-23 21:47:58 -0800

    product component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import data from './data';
-
+import Product from './components/Product';
 function App() {
   return (
     <div className = "grid-container">
@@ -16,27 +16,9 @@ function App() {
     </header>
     <main>
         <div className = "row center" >
-          {
-            data.products.map(product=>{
+          {data.products.map(product=>{
               return (
-                <div key ={product.id} className ="card" >
-                <a href = {`/product/${product._id}`} >
-                    <img className = "medium" src = {product.image} alt = {product.name}/>
-                </a>
-                <div className="card-body" >
-                <a href = {`/product/${product._id}`} >
-                        <h2>{product.name}</h2>
-                    </a>
-                    <div className ="rating">
-                        <span><i className ="fa fa-star"></i></span>
-                        <span><i className ="fa fa-star"></i></span>
-                        <span><i className ="fa fa-star"></i></span>
-                        <span><i className ="fa fa-star"></i></span>
-                        <span><i className ="fa fa-star"></i></span>
-                    </div>
-                    <div className ="price" >${product.price}</div>
-                </div>
-            </div>
+                <Product key={product._id} product ={product}/>
               )
             })
           }
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Product({product}) {
+    return (
+        <div key ={product.id} className ="card" >
+        <a href = {`/product/${product._id}`} >
+            <img className = "medium" src = {product.image} alt = {product.name}/>
+        </a>
+        <div className="card-body" >
+        <a href = {`/product/${product._id}`} >
+                <h2>{product.name}</h2>
+            </a>
+            <div className ="rating">
+                <span><i className ="fa fa-star"></i></span>
+                <span><i className ="fa fa-star"></i></span>
+                <span><i className ="fa fa-star"></i></span>
+                <span><i className ="fa fa-star"></i></span>
+                <span><i className ="fa fa-star"></i></span>
+            </div>
+            <div className ="price" >${product.price}</div>
+        </div>
+    </div>
+    )
+};
