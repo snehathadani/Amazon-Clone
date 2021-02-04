51adb14a4b277d79e83fdb78b3558fe3c1f59a71
snehathadani <snehacopper@gmail.com>
2021-02-04 13:02:11 -0800

    server, userRouter changed

--- start_mongo.sh
+++ start_mongo.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-docker rm mongo && docker run --name mongo -v d:/Docker/Volumes/mongo/amazon:/data/db -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=secret mongo
+docker rm mongo 
+docker run --name mongo -v c:/Docker/Volumes/mongo/amazon:/data/db -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=secret mongo
