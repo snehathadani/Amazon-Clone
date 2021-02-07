923af73a1267df97aaafbe173f0b90bde25d8747
snehathadani <snehacopper@gmail.com>
2021-02-06 19:09:15 -0800

    Shipping Column with address details

---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import CheckoutSteps from '../components/CheckoutSteps'
+
+export default function PlaceOrderScreen(props) {
+    const cart = useSelector(state=> state.cart);//import cart
+//if user didn't enter payment method he should be redirected to payment screen
+    if(!cart.paymentMethod){
+        props.history.push('/payment');
+    }
+    return (
+        <div>
+            <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
+            <div className="row top">
+                <div className="col-2">
+                    <ul>
+                        <li>
+                            <div className="card card-body">
+                                <h2>Shipping</h2>
+                                <p>
+                                    <strong>Name:</strong>
+                                    {cart.shippingAddress.fullName} <br/>
+                                    <strong>Address:</strong>
+                                    {cart.shippingAddress.address}
+                                    {cart.shippingAddress.city}
+                                    {cart.shippingAddress.postalCode}
+                                    {cart.shippingAddress.country}
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-1"></div>
+            </div>
+        </div>
+    )
+}
