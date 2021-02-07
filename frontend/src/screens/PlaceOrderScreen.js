89400d9d9478289017b58c4bc5490893832ca9f4
snehathadani <snehacopper@gmail.com>
2021-02-07 13:53:22 -0800

    dispatch createOrder with orderItems

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
 import CheckoutSteps from '../components/CheckoutSteps'
 //create shipping payment and order details section
@@ -17,7 +17,13 @@ export default function PlaceOrderScreen(props) {
     cart.taxPrice = toPrice(0.15 * cart.itemsPrice);
     cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
 
-    const placeOrderHandler = ()=>{}
+    const dispatch = useDispatch();
+
+    const placeOrderHandler = ()=>{
+        //in the backend we have named and expecting orserItems inside orderRouter
+        //so replace cartItems with orderItems
+        dispatch(createOrder({...cart, orderItems:cart.cartItems}))
+    }
 
     return (
         <div>
